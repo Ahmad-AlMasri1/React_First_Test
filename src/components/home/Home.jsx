d6396409397c4366b74bea7fc73d13a07f48ff0a
Ahmad-AlMasri1 <ahmad.mcksj@gmail.com>
2026-05-20 18:50:45 +0300

    add hero section to Home Page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 import './Home.css';
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <div
+        class="hero min-h-screen bg-[url(https://images5.alphacoders.com/939/thumb-1920-939055.jpg)]"
+        
+      >
+        <div class="hero-overlay"></div>
+        <div class="hero-content text-neutral-content text-center">
+          <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+            <p class="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+          
+    </>
   )
 }
 
